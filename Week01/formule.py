90695dcce28ff29baa23dee3205d730ec9263dbd
dantecoopman <dante.coopman@student.howest.be>
2017-09-26 10:21:04 +0200

    eerste oefening

---
+++ Week01/formule.py
@@ -0,0 +1,6 @@
+getal1 = int(input("Geef een getal op: "))
+getal2 = int(input("Geef een getal op: "))
+
+formule = (getal1 + getal2) * (getal1 + getal2)
+
+print("({0} + {1}) ^ 2 = {2}".format(getal1, getal2, formule))
